581ac905f4fcea5de6f5da7c37db93666999c50d
odlzie <liuzhao9ster@gmail.com>
2023-08-26 00:39:32 +0100

    add input

--- action1/hello_world.py
+++ action1/hello_world.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     my_input = os.environ['INPUT_WHO-TO-GREET']
-    my_output = f'Hello world'
+    my_output = f'Hello {my_input}'
     print(f'::set-output name=myprint::{my_output}')
 
 if __name__ == '__main__':
