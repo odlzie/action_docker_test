1a3a57249beddda2cdb7a9fcd6cf01a595e693b3
odlzie <liuzhao9ster@gmail.com>
2023-08-26 00:47:46 +0100

    add input

--- action1/hello_world.py
+++ action1/hello_world.py
@@ -4,7 +4,11 @@ import os
 def main():
     my_input = os.environ['INPUT_WHO-TO-GREET']
     my_output = f'Hello {my_input}'
-    print(f'::set-output name=myprint::{my_output}')
+    # print(f'::set-output name=myprint::{my_output}')
+
+    output_name = 'myprint'
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{output_name}={my_output}', file=fh)
 
 if __name__ == '__main__':
     main()
